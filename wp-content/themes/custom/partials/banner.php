97f857bee404322596eade34cbd6230e939f8d25
Janis Mozais <janis.mozais@inbox.lv>
2017-12-04 14:48:48 +0200

    ...

--- wp-content/themes/custom/partials/banner.php
+++ wp-content/themes/custom/partials/banner.php
@@ -39,19 +39,33 @@
                 </div>
 				<div class="titles">
                     <div class="top-first">
-                        <div class="first-title">ssssssssss</div>
+                        <div class="first-title" style="width: 200px; margin: 0 auto;    margin-bottom: 40px;">
+	                        <?php echo __( 'Когенерационная станция - электрическая мощность 4,5 МWТ - тепловая мощность 14,74 МWТ', 'preico' ) ?>
+                        </div>
                     </div>
+
+
                     <div class="top-second">
-                        <div class="second-title">ssssssssss</div>
-                        <div class="third-title">ssssssssss</div>
+                        <div class="second-title" style="width: 140px; text-align: center;margin-left: -19px;">
+	                        <?php echo __( 'Mайнинг - фермa 4,5 MWT 6750 Th/s', 'preico' ) ?>
+                        </div>
+                        <div class="third-title" style="width: 190px;text-align: center;margin-right: -39px;">
+	                        <?php echo __( 'Тепличный комплекс выращивание 2 млн саженцев ели /год', 'preico' ) ?>
+                        </div>
                     </div>
 
                     <div class="bottom-first">
-                        <div class="fourth-title">ssssssssss</div>
-                        <div class="fifth-title">ssssssssss</div>
+                        <div class="fourth-title" style="width: 150px;margin-left: -27px;">
+	                        <?php echo __( 'Лесные ресурсы - площадь лесного фонда 71 967 га', 'preico' ) ?>
+                        </div>
+                        <div class="fifth-title" style="width: 130px;margin-right: -10px;">
+	                        <?php echo __( 'Лесозавод - мощность 120 000 м3/год', 'preico' ) ?>
+                        </div>
                     </div>
                     <div class="bottom-second">
-                        <div class="sixth-title">ssssssssss</div>
+                        <div class="sixth-title" style="width: 200px;margin: 0 auto;">
+	                        <?php echo __( 'Завод древесных пеллет - мощность 120 000 тонн/год', 'preico' ) ?>
+                        </div>
                     </div>
 				</div>
 			</div>
