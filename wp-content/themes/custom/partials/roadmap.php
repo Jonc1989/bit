4c25f3c014f30cd6f6984b857daa58095d8a10d6
janis.mozais@inbox.lv <janis.mozais@inbox.lv>
2017-12-29 12:10:10 +0200

    ...

--- wp-content/themes/custom/partials/roadmap.php
+++ wp-content/themes/custom/partials/roadmap.php
@@ -4,14 +4,14 @@
     <div class="container">
         <div class="col-md-6 col-sm-6 left-block">
             <div class="roadmap-block first">
-                <h4 class="yellow">2017 Q2-Q4 - Разработка проектной базы</h4>
+                <h4 class="yellow"><?php echo __( '2017 Q2-Q4 - Разработка проектной базы', 'preico' ) ?></h4>
 
                 <div class="roadmap-item">
                     <div class="li-img-wrap">
                         <img src="<?php bloginfo('template_directory');?>/resources/images/peregovor.png">
 
                     </div>
-                    <p class="title-text">Проведение переговоров с поставщиками</p>
+                    <p class="title-text"><?php echo __( 'Проведение переговоров с поставщиками', 'preico' ) ?></p>
                 </div>
 
                 <div class="roadmap-item">
@@ -19,19 +19,19 @@
                         <img src="<?php bloginfo('template_directory');?>/resources/images/zapusk_sajta.png">
 
                     </div>
-                    <p class="title-text">Запуск сайта</p>
+                    <p class="title-text"><?php echo __( 'Запуск сайта', 'preico' ) ?></p>
                 </div>
             </div>
 
             <div class="roadmap-block second hidden-lg hidden-md hidden-sm">
-                <h4 class="yellow">2018 Q1  - PRE SALE </h4>
+                <h4 class="yellow"><?php echo __( '2018 Q1  - PRE SALE', 'preico' ) ?></h4>
 
                 <div class="roadmap-item">
                     <div class="li-img-wrap">
                         <img src="<?php bloginfo('template_directory');?>/resources/images/marketing.png">
 
                     </div>
-                    <p class="title-text">Маркетинговая составляющая</p>
+                    <p class="title-text"><?php echo __( 'Маркетинговая составляющая', 'preico' ) ?></p>
                 </div>
 
                 <div class="roadmap-item">
@@ -39,33 +39,33 @@
                         <img src="<?php bloginfo('template_directory');?>/resources/images/registracija.png">
 
                     </div>
-                    <p class="title-text">Пегистрационные процессы</p>
+                    <p class="title-text"><?php echo __( 'Пегистрационные процессы', 'preico' ) ?></p>
                 </div>
 
                 <div class="roadmap-item">
                     <div class="li-img-wrap">
                         <img src="<?php bloginfo('template_directory');?>/resources/images/video.png">
 
-                    </div> <p class="title-text">Установка видеонаблюдения</p>
+                    </div> <p class="title-text"><?php echo __( 'Установка видеонаблюдения', 'preico' ) ?></p>
                 </div>
 
                 <div class="roadmap-item">
                     <div class="li-img-wrap">
                         <img src="<?php bloginfo('template_directory');?>/resources/images/rabota_s_investorami.png">
 
-                    </div><p class="title-text">Работа с инвесторами</p>
+                    </div><p class="title-text"><?php echo __( 'Работа с инвесторами', 'preico' ) ?></p>
                 </div>
             </div>
 
             <div class="roadmap-block third">
-                <h4 class="yellow">2018 Q2 - ICO</h4>
+                <h4 class="yellow"><?php echo __( '2018 Q2 - ICO', 'preico' ) ?></h4>
 
                 <div class="roadmap-item">
                     <div class="li-img-wrap">
                         <img src="<?php bloginfo('template_directory');?>/resources/images/podgotovitelnie_raboti.png">
 
                     </div>
-                    <p class="title-text">Подготовительные работы</p>
+                    <p class="title-text"><?php echo __( 'Подготовительные работы', 'preico' ) ?></p>
                 </div>
 
                 <div class="roadmap-item">
@@ -73,13 +73,13 @@
                         <img src="<?php bloginfo('template_directory');?>/resources/images/pokupka.png">
 
                     </div>
-                    <p class="title-text">Покупка техникит</p>
+                    <p class="title-text"><?php echo __( 'Покупка техники', 'preico' ) ?></p>
                 </div>
                 <div class="roadmap-item">
                     <div class="li-img-wrap">
                         <img src="<?php bloginfo('template_directory');?>/resources/images/stroitelstvo.png">
 
-                    </div><p class="title-text">Начало строительных работ</p>
+                    </div><p class="title-text"><?php echo __( 'Начало строительных работ', 'preico' ) ?></p>
 
                 </div>
                 <div class="roadmap-item">
@@ -87,7 +87,7 @@
                         <img src="<?php bloginfo('template_directory');?>/resources/images/pokupka.png">
 
                     </div>
-                    <p class="title-text">Покупка производственного оборудования</p>
+                    <p class="title-text"><?php echo __( 'Покупка производственного оборудования', 'preico' ) ?></p>
                 </div>
             </div>
 
@@ -95,14 +95,14 @@
 
         <div class="col-md-6 col-sm-6 right-block hidden-xs">
             <div class="roadmap-block second">
-                <h4 class="yellow">2018 Q1  - PRE SALE </h4>
+                <h4 class="yellow"><?php echo __( '2018 Q1  - PRE SALE', 'preico' ) ?> </h4>
 
                 <div class="roadmap-item">
                     <div class="li-img-wrap">
                         <img src="<?php bloginfo('template_directory');?>/resources/images/marketing.png">
 
                     </div>
-                    <p class="title-text">Маркетинговая составляющая</p>
+                    <p class="title-text"><?php echo __( 'Маркетинговая составляющая', 'preico' ) ?></p>
                 </div>
 
                 <div class="roadmap-item">
@@ -110,34 +110,34 @@
                         <img src="<?php bloginfo('template_directory');?>/resources/images/registracija.png">
 
                     </div>
-                    <p class="title-text">Пегистрационные процессы</p>
+                    <p class="title-text"><?php echo __( 'Пегистрационные процессы', 'preico' ) ?></p>
                 </div>
 
                 <div class="roadmap-item">
                     <div class="li-img-wrap">
                         <img src="<?php bloginfo('template_directory');?>/resources/images/video.png">
 
-                    </div> <p class="title-text">Установка видеонаблюдения</p>
+                    </div> <p class="title-text"><?php echo __( 'Установка видеонаблюдения', 'preico' ) ?></p>
                 </div>
 
                 <div class="roadmap-item">
                     <div class="li-img-wrap">
                         <img src="<?php bloginfo('template_directory');?>/resources/images/rabota_s_investorami.png">
 
-                    </div><p class="title-text">Работа с инвесторами</p>
+                    </div><p class="title-text"><?php echo __( 'Работа с инвесторами', 'preico' ) ?></p>
                 </div>
             </div>
         </div>
 
         <div class="col-md-12 col-sm-12 bottom-block">
             <div class="roadmap-block fourth">
-                <h4 class="yellow">2019 Q2-Q4 - Запуск комплекса</h4>
+                <h4 class="yellow"><?php echo __( '2019 Q2-Q4 - Запуск комплекса', 'preico' ) ?></h4>
 
                 <div class="roadmap-item">
                     <div class="li-img-wrap">
                         <img src="<?php bloginfo('template_directory');?>/resources/images/nachalo_prodaz.png">
 
-                    </div><p class="title-text">Начало продаж продукции компании</p>
+                    </div><p class="title-text"><?php echo __( 'Начало продаж продукции компании', 'preico' ) ?></p>
 
                 </div>
 
@@ -145,7 +145,7 @@
                     <div class="li-img-wrap">
                         <img src="<?php bloginfo('template_directory');?>/resources/images/vikup_tokena.png">
 
-                    </div><p class="title-text">Начало выкупа токенов</p>
+                    </div><p class="title-text"><?php echo __( 'Начало выкупа токенов', 'preico' ) ?></p>
 
                 </div>
             </div>
