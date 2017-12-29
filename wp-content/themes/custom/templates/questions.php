cf21cb6d77ccb644e7ba96964a8cd3c0d35433fa
janis.mozais@inbox.lv <janis.mozais@inbox.lv>
2017-12-29 18:18:06 +0200

    questions css

--- wp-content/themes/custom/templates/questions.php
+++ wp-content/themes/custom/templates/questions.php
@@ -8,7 +8,41 @@ get_header(); ?>
 
         <?php get_template_part('partials/menu');?>
 
+        <div class="container">
+
+
+            <a class="" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+                <div class="question-toggle">
+                    Question
+                    <span>
+                        <img class="bullet" src="<?php bloginfo('template_directory');?>/resources/images/yellow-arrow.png">
+                    </span>
+                </div>
+            </a>
+
+            <div class="collapse" id="collapseExample">
+                <div class="questions">
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+                </div>
+            </div>
+
+            <a class="" data-toggle="collapse" href="#collapseExample2" role="button" aria-expanded="false" aria-controls="collapseExample2">
+                <div class="question-toggle">
+                    Question2
+                    <span>
+                        <img class="bullet" src="<?php bloginfo('template_directory');?>/resources/images/yellow-arrow.png">
+                    </span>
+                </div>
+            </a>
+
+            <div class="collapse" id="collapseExample2">
+                <div class="questions">
+                    Anim pariatur cliche repreh endfgg fgeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+                </div>
+            </div>
         </div>
+
+    </div>
 
 
 </div>
